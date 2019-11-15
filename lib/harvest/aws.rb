67745c0271009f61404b3523104283dbef7d93a2
gen <ymdtshm@gmail.com>
2019-11-15 16:37:50 +0000

    add check state

--- lib/harvest/aws.rb
+++ lib/harvest/aws.rb
@@ -15,6 +15,7 @@ module Harvest
         ec2.instances.map do |i|
           host = i.tags.find{|h| h.key == 'Name' }.value
           next unless host =~ /#{query}/
+          next unless i.state.name == "running"
 
           addr_type = opts[:address_type] || :public
           type      = opts[:type] || :linux
@@ -24,6 +25,7 @@ module Harvest
             type:       type,
             host:       host,
             host_name:  ip_addr,
+            delegator:  i
           }
         end
       end
